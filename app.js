22ae85e115287ce481623ed102ead310c3cf42de
Saim123321 <saimasimkhan8@gmail.com>
2026-02-11 14:47:16 +0500

    deployment

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ const leaderboardRoutes = require('./api/routers/leaderboard.router');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true
+}));
 
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.path}`);
